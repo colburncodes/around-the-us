b79655f9a835760cfad483c2ea5e1bae54dd4e87
colburncodes <colburnsanders@gmail.com>
2023-02-16 13:51:56 -0600

    feat: add functionality to like and delete a card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,21 +3,29 @@ import heart from "../images/heart.svg";
 import { CurrentUserContext } from "../context/CurrentUserContext";
 
 export function Card(props) {
-  const { card, onCardClick } = props;
-  const user = React.useContext(CurrentUserContext);
+  const { card, onCardClick, onCardLike, onCardDelete } = props;
+  const currentUser = React.useContext(CurrentUserContext);
 
-  const isLiked = card.likes.some((i) => i._id === user._id);
+  const isLiked = card.likes.some((user) => user._id === currentUser._id);
   const cardLikeButtonClassName = `card__like-button ${
     isLiked && "card__like-active"
   }`;
 
-  const isOwn = card.owner._id === user._id;
+  const isOwn = card.owner._id === currentUser._id;
   const cardDeleteButtonClassName = `card__delete-button ${
     isOwn ? "card__delete-button_visible" : "card__delete-button_hidden"
   }`;
 
   function handleClick() {
     onCardClick(card);
+  }
+
+  function handleLikeClick() {
+    onCardLike(card);
+  }
+
+  function handleDeleteClick() {
+    onCardDelete(card);
   }
 
   return (
@@ -31,12 +39,13 @@ export function Card(props) {
       <div className="card__label">
         <h2 className="card__label-text">{card.name}</h2>
         <button
-          className="card__delete-button"
+          onClick={handleDeleteClick}
+          className={cardDeleteButtonClassName}
           type="button"
           aria-label="delete"
         ></button>
         <button className={cardLikeButtonClassName} type="button">
-          <img src={heart} alt="heart" />
+          <img src={heart} alt="heart" onClick={handleLikeClick} />
         </button>
         <p className="card__like-count">
           {card.likes.length > 0 ? card.likes.length : ""}
