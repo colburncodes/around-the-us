3589035e3af181e8530e80687442bce2daceb93b
colburncodes <colburnsanders@gmail.com>
2023-03-19 11:33:21 -0500

    fixed css issues with header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 export function Footer() {
   return (
     <footer className="footer page__section">
-      <p className="footer__title">&copy; 2022 Around The U.S.</p>
+      <p className="footer__title">
+        &copy; 2023 Colburn Sanders - Around The U.S.
+      </p>
     </footer>
   );
 }
