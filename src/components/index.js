69f7e771d4a096d7b00b2ac9ae64006e92e90906
colburncodes <colburnsanders@gmail.com>
2023-03-21 17:21:50 -0500

    cleaned up import statements

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { Header } from "./Header";
+export { Main } from "./Main";
+export { Footer } from "./Footer";
+export { ImagePopup } from "./ImagePopup";
+export { EditProfilePopup } from "./Forms/EditProfilePopup";
+export { EditAvatarPopup } from "./Forms/EditAvatarPopup";
+export { AddPlacePopup } from "./Forms/AddPlacePopup";
+export { DeleteConfirmationModal } from "./DeleteConfirmationModal";
