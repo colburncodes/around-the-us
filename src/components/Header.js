cbeb4608450afc0a79e15af8f9ac48556b383453
colburncodes <colburnsanders@gmail.com>
2023-05-21 19:03:06 -0500

    fix: :recycle: fixed login/register/signout tested locally

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import headerLogo from "../images/logo.svg";
 import { Route, Link } from "react-router-dom";
 
-export function Header({ onSignOut, email }) {
+export function Header({ onSignOut, email, isLoggedIn }) {
   function handleSignOut() {
     onSignOut();
   }
@@ -10,23 +10,28 @@ export function Header({ onSignOut, email }) {
     <header className="header page__section">
       <img className="header__logo" src={headerLogo} alt="Around the US" />
       <Route path="/">
-        <div className="header__container">
-          <p className="header__user">{email}</p>
-          {/* <button className="header__logout" onClick={handleSignOut}>
-            Logout
-          </button> */}
-
-          <Route path="/signup">
-            <Link className="header__auth-link" to="signup">
-              Sign up
-            </Link>
-          </Route>
-          <Route path="/signin">
-            <Link className="header__auth-link" to="signin">
-              Log in
-            </Link>
-          </Route>
-        </div>
+        {isLoggedIn ? (
+          <div className="header__container">
+            <p className="header__user">{email}</p>
+            <button className="header__logout" onClick={handleSignOut}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="header__container">
+            <p className="header__user">{email}</p>
+          </div>
+        )}
+      </Route>
+      <Route path="/signup">
+        <Link className="header__auth-link" to="signup">
+          Sign up
+        </Link>
+      </Route>
+      <Route path="/signin">
+        <Link className="header__auth-link" to="signin">
+          Log in
+        </Link>
       </Route>
     </header>
   );
